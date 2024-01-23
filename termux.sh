a77c9946f88288b489a305abe7337ce67e6f049e
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2024-01-23 20:47:31 +0330

    update termux.sh

--- termux.sh
+++ termux.sh
@@ -53,6 +53,7 @@ socks() {
    echo -e "${green}socks://Og==@127.0.0.1:8086#Free Warp %28usef%29${rest}"
    echo "================================================"
    echo -e "${yellow}To run again, type:${green} warp ${rest}or${green} usef ${rest}"
+   echo "================================================"
    echo ""
 }
 
