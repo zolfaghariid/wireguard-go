81b0f77a8c04aa5d15dd206f98c5396989044765
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2024-02-10 12:28:13 +0330

    update termux.sh

--- termux.sh
+++ termux.sh
@@ -68,7 +68,7 @@ install() {
     pacman -Syu openssh = apt update; apt full-upgrade -y; apt install -y openssh
     check_dependencies
 
-    if wget https://github.com/bepass-org/wireguard-go/releases/download/v1.0.1-rc1-alpha/warp-android-arm64.9f90b7.zip &&
+    if wget https://github.com/bepass-org/wireguard-go/releases/download/v1.0.1-rc1/warp-android-arm64.9f90b7.zip &&
         unzip warp-android-arm64.9f90b7.zip &&
         chmod +x warp &&
         cp warp "$PREFIX/bin/usef" &&
@@ -104,7 +104,7 @@ install_arm() {
         *) echo -e "${red}Unsupported architecture.${rest}"; return ;;
     esac
 
-    WARP_URL="https://github.com/bepass-org/wireguard-go/releases/download/v1.0.1-rc1-alpha/warp-linux-$ARCH.9f90b7.zip"
+    WARP_URL="https://github.com/bepass-org/wireguard-go/releases/download/v1.0.1-rc1/warp-linux-$ARCH.9f90b7.zip"
 
     if wget "$WARP_URL" &&
         unzip "warp-linux-$ARCH.9f90b7.zip" &&
